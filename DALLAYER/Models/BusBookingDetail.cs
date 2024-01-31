3569df7b6b967cc1782e40a353e35904fb0bd8b9
Maheshvema <babumahesh2088@gmail.com>
2024-01-31 17:05:16 +0530

    add

---
+++ DALLAYER/Models/BusBookingDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DALLAYER.Models
+{
+    public partial class BusBookingDetail
+    {
+        public string BusId { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public int TicketId { get; set; }
+        public string FullName { get; set; } = null!;
+        public int Age { get; set; }
+        public string ContactNumber { get; set; } = null!;
+        public string EmailId { get; set; } = null!;
+        public int NumberOfTickets { get; set; }
+        public int TotalAmount { get; set; }
+        public int Fare { get; set; }
+        public string PaymentMode { get; set; } = null!;
+        public string? PaymentStatus { get; set; }
+        public string? TicketStatus { get; set; }
+    }
+}
